086ca07d4b2bcfd40046131ec1c101b99f23b927
chiyuan9 <chiyuan0913@163.com>
2023-07-05 19:35:33 -0400

    finish si in sdb

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -47,6 +47,12 @@ static int cmd_c(char *args) {
   return 0;
 }
 
+static int cmd_si(char *args) {
+  int n = atoi(args);
+  cpu_exec(n);
+  return 0;
+}
+
 
 static int cmd_q(char *args) {
   return -1;
@@ -62,6 +68,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Execute N instructions in single-step", cmd_si },
 
   /* TODO: Add more commands */
 
