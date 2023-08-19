984e8ab0dc76139e88645fa4d444375d1e8c8a35
chiyuan9 <chiyuan0913@163.com>
2023-08-19 19:31:08 -0400

    realize string.c

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,11 +5,24 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t count = 0;
+  
+  while(*s!='\0')
+  {
+    count++;
+    s++;
+  }
+  return count;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+   char *saved = dst;
+   while (*src)
+   {
+       *dst++ = *src++;
+   }
+   *dst = 0;
+   return saved;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +30,17 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+    strcpy(dst + strlen(dst), src);
+    return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while(*s1 && (*s1 == *s2))
+    {
+        s1++;
+        s2++;
+    }
+    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +48,10 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  register unsigned char *ptr = (unsigned char*)s;
+  while (n-- > 0)
+    *ptr++ = c;
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +63,21 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char*  p1   = s1;
+    const unsigned char*  end1 = p1 + n;
+    const unsigned char*  p2   = s2;
+    int                   d = 0;
+    for (;;) {
+        if (d || p1 >= end1) break;
+        d = (int)*p1++ - (int)*p2++;
+        if (d || p1 >= end1) break;
+        d = (int)*p1++ - (int)*p2++;
+        if (d || p1 >= end1) break;
+        d = (int)*p1++ - (int)*p2++;
+        if (d || p1 >= end1) break;
+        d = (int)*p1++ - (int)*p2++;
+    }
+    return d;
 }
 
 #endif
